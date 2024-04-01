62eb6f3a5d25c7f568774826185fccec09aaa0c2
Prokop Vašák <prokop.vasak.021@pslib.cz>
2024-04-01 13:21:19 +0200

    nějaká chyby v EVALUATE_CARDS

--- NotBalatro/src/components/GameContext.tsx
+++ NotBalatro/src/components/GameContext.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren, createContext, useReducer } from 'react';
 import { Player, Enemy, Card, Joker, State, Action, CardNumber, Combo } from './types';
-import { Pack } from "./Data";
+import { Pack, Combos } from "./Data";
 import { useState } from 'react';
 
 
@@ -18,10 +18,10 @@ const reducer = (state: State, action: Action) => {
     switch (action.type) {
         case "EVALUATE_CARDS":
             //A,2,3,4,5,6,7,8,9,10,J,Q,K
-            const values: number[] = [];
+            const values: number[] = [0,0,0,0,0,0,0,0,0,0,0,0,0];
             //spades,hearts,diamonds,clubs
-            const types: number[] = [];
-            const getBetsCombo = () => {
+            const types: number[] = [0,0,0,0];
+            const getBestCombo = () => {
                 action.cards.forEach(element => {
                     switch (element.type) {
                         case "Spades":
@@ -78,119 +78,131 @@ const reducer = (state: State, action: Action) => {
                             values[12]++;
                             break;
                     } 
-                    const checkFiveConsecutiveOnes = () => {
-                        let count = 0;
-                        for (let i = 0; i < values.length; i++) {
-                            if (values[i] === 1) {
-                                count++;
-                                if (count === 5) {
-                                    return true;
-                                }
-                            } else {
-                                count = 0;
-                            }
-                        }
-                        return false;
-                    };
-                    const checkFiveSameType = () => {
-                        types.forEach(element => {
-                            if (element === 5) {
-                                return true;
-                            }
-                        });
-                        return false;
-                    }
-
-                    const checkFourOfAKind = () => {
-                        for (let i = 0; i < values.length; i++) {
-                            if (values[i] === 4) {
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-
-                    const checkFullHouse = () => {
-                        let three = false;
-                        let two = false;
-                        for (let i = 0; i < values.length; i++) {
-                            if (values[i] === 3) {
-                                three = true;
-                            }
-                            if (values[i] === 2) {
-                                two = true;
-                            }
-                        }
-                        return three && two;
-                    }
-
-                    const checkThreeOfAKind = () => {
-                        for (let i = 0; i < values.length; i++) {
-                            if (values[i] === 3) {
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-
-                    const checkTwoPairs = () => {
-                        let pairs = 0;
-                        for (let i = 0; i < values.length; i++) {
-                            if (values[i] === 2) {
-                                pairs++;
-                            }
-                        }
-                        return pairs === 2;
-                    }
-                    const checkPair = () => {
-                        for (let i = 0; i < values.length; i++) {
-                            if (values[i] === 2) {
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-
-                    if(checkFiveConsecutiveOnes()&&checkFiveSameType()){
-                        return "straight flush"
-                    }
-
-                    if(checkFourOfAKind()){
-                        return "four of a kind"
-                    }
-
-                    if (checkFullHouse()) {
-                        return "full house"
-                    }
-
-                    if (checkFiveSameType()) {
-                        return "flush"
-                    }
-
-                    if (checkFiveConsecutiveOnes()) {
-                        return "straight"
-                    }
-
-                    if (checkThreeOfAKind()) {
-                        return "three of a kind"
-                    }
-
-                    if (checkTwoPairs()){
-                        return "two pair"
-                    }
-
-                    if (checkPair()) {
-                        return "pair"
-                    }
                     
-                    else return "high card";
-
+                    console.log(values);
+                    console.log(types);
 
 
                                                              
                 });
+                const checkFiveConsecutiveOnes = () => {
+                    let count = 0;
+                    for (let i = 0; i < values.length; i++) {
+                        if (values[i] === 1) {
+                            count++;
+                            if (count === 5) {
+                                return true;
+                            }
+                        } else {
+                            count = 0;
+                        }
+                    }
+                    return false;
+                };
+                const checkFiveSameType = () => {
+                    types.forEach(element => {
+                        if (element === 5) {
+                            return true;
+                        }
+                    });
+                    return false;
+                }
+
+                const checkFourOfAKind = () => {
+                    for (let i = 0; i < values.length; i++) {
+                        if (values[i] === 4) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+
+                const checkFullHouse = () => {
+                    let three = false;
+                    let two = false;
+                    for (let i = 0; i < values.length; i++) {
+                        if (values[i] === 3) {
+                            three = true;
+                        }
+                        if (values[i] === 2) {
+                            two = true;
+                        }
+                    }
+                    return three && two;
+                }
+
+                const checkThreeOfAKind = () => {
+                    for (let i = 0; i < values.length; i++) {
+                        if (values[i] === 3) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+
+                const checkTwoPairs = () => {
+                    let pairs = 0;
+                    for (let i = 0; i < values.length; i++) {
+                        if (values[i] === 2) {
+                            pairs++;
+                        }
+                    }
+                    return pairs === 2;
+                }
+                const checkPair = () => {
+                    for (let i = 0; i < values.length; i++) {
+                        if (values[i] === 2) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+
+                if(checkFiveConsecutiveOnes()&&checkFiveSameType()){
+                    return "straight flush"
+                }
+
+                if(checkFourOfAKind()){
+                    return "four of a kind"
+                }
+
+                if (checkFullHouse()) {
+                    return "full house"
+                }
+
+                if (checkFiveSameType()) {
+                    return "flush"
+                }
+
+                if (checkFiveConsecutiveOnes()) {
+                    return "straight"
+                }
+
+                if (checkThreeOfAKind()) {
+                    return "three of a kind"
+                }
+
+                if (checkTwoPairs()){
+                    return "two pair"
+                }
+
+                if (checkPair()) {
+                    return "pair"
+                }
+                
+                else return "high card";
             }
-            return { ...state };
+
+            const combo: Combo =  Combos.find(c => c.name === getBestCombo())!;
+            const newEnemyScore = state.enemy.score - (combo.baseMult * combo.baseChips);
+            return {
+                ...state,
+                enemy: {
+                    ...state.enemy,
+                    score: newEnemyScore
+                }
+            };
+            
         default:
             return state;
     }
